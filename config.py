29b6426605c2692a29c8be176eb445cdd220915b
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-11-25 23:52:06 -0500

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ bot = telebot.TeleBot(TOKEN, threaded=True, num_threads=5)
 Base = declarative_base()
 engine = create_engine(
     os.getenv("postgresql://postgres:5e3*5bEfgF*f3C-6-AaGg-2f33333GE*@viaduct.proxy.rlwy.net:33628/railway"),
-    echo=False)
+    echo=True)
 
 # import importdir
 # importdir.do("handlers", globals())
